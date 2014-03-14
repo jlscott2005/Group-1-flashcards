5ead7d7ebb1cdc1d57a96dd6a3fbf7562f77556b
Apprentice <apprentice@dbc-chicago-028.local>
2014-03-14 15:07:01 -0500

    implement answer testing \!

--- flashcards/app/controllers/index.rb
+++ flashcards/app/controllers/index.rb
@@ -10,17 +10,26 @@ end
 
 get '/decks/:deck_id/:card' do
   @deck = Deck.find(params[:deck_id]).cards
+
   @card = @deck[params[:card].to_i]
   @deck_id = params[:deck_id]
-  @card_id = params[:card].to_i + 1
+  @card_id = params[:card].to_i
   erb :card
 end
 
 post '/decks/:deck_id/:card' do
   @deck_id = params[:deck_id]
   @deck = Deck.find(params[:deck_id]).cards
+
   @card = @deck[params[:card].to_i]
-  @card_id = params[:card].to_i + 1
+  if params[:guess].upcase == @card.answer.upcase
+    @msg = "CORRECT!"
+    @card_id = params[:card].to_i + 1
+    @card = @deck[@card_id]
+  else
+    @msg = "Sorry, try again."
+    @card_id = params[:card].to_i
+  end
   erb :card
 end
 
