5098c0bc88ac641e8d2eb4ae73d902cd22077eba
Apprentice <apprentice@dbc-chicago-028.local>
2014-03-14 18:42:01 -0500

    haxed and ajaxed

--- flashcards/public/js/application.js
+++ flashcards/public/js/application.js
@@ -3,6 +3,37 @@ $(document).ready(function() {
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
+  var deckID = function(string) {
+    return string.match(/deck_id=(\d)+/)[1];
+  };
+
+  var cardID = function(string) {
+    return string.match(/card=(\d)+/)[1];
+  };
+
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-  $()
+  $('#guess').on("submit", function(event){
+      event.preventDefault();
+      var result = $(this).serialize();
+      var url = '/decks/' + deckID(result) + '/' + cardID(result);
+      $.post(url, result, function(response) {
+        $('#msg').text(response);
+        setTimeout(function () {
+           newCard = parseInt(cardID(result)) + 1
+           var url = '/decks/' + deckID(result) + '/' + newCard;
+           console.log(url)
+           window.location.href = url
+        }, 2000);
+      });
+  });
+
+
+
+
+
+
+
+
+
+
 });
